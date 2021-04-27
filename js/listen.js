980fc17aaff135aafee6b5d7b809eb0349a45784
Easonfans2021 <721849941@qq.com>
2021-04-27 19:25:57 +0800

    信息

---
+++ js/listen.js
@@ -0,0 +1,7 @@
+myVid=document.getElementById("login");
+/*myVid.addEventListener('ended',function(){
+  window.location.href="index.html";
+});*/
+if(myVid){
+  window.location.href="www.baidu.com";
+}
